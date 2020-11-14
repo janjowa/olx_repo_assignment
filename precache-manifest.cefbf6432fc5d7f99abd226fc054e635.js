8836fac79d88561165252c7655d7908eec079649
janjowa <65031072+janjowa@users.noreply.github.com>
2020-11-14 20:45:46 +0500

    Add files via upload

---
+++ precache-manifest.cefbf6432fc5d7f99abd226fc054e635.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f88a5933eb79158f2d4ea09bdabe77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d705c9d7916325acbf",
+    "url": "/static/css/main.6d3280c4.chunk.css"
+  },
+  {
+    "revision": "ab41fc9e944046806481",
+    "url": "/static/js/2.d4b43720.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4b43720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d705c9d7916325acbf",
+    "url": "/static/js/main.d23885c4.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "667abcf8c7dc4ddfb15d75a49824e138",
+    "url": "/static/media/fysisoft_OLX_.667abcf8.png"
+  }
+]);
